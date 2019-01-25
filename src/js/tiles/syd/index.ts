096845adbc92af3d56499f6833b32b3dfb9ea8e2
Tomas Machalek <tomas@machalek.xyz>
2019-01-25 16:28:19 +0100

    Add template for syd tile

---
+++ src/js/tiles/syd/index.ts
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2019 Tomas Machalek <tomas.machalek@gmail.com>
+ * Copyright 2019 Institute of the Czech National Corpus,
+ *                Faculty of Arts, Charles University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { TileFactory, ITileProvider, QueryType } from '../../abstract/types';
+import { ActionDispatcher, ViewUtils } from 'kombo';
+import { GlobalComponents } from '../../views/global';
+import { AppServices } from '../../appServices';
+import {init as viewInit} from './view';
+import { SydModel } from './model';
+import { WdglanceMainFormModel } from '../../models/query';
+import { SyDAPI } from './api';
+
+declare var require:any;
+require('./style.less');
+
+export interface SyDTileConf {
+    apiURL:string;
+}
+
+/**
+ *
+ */
+export class SyDTile implements ITileProvider {
+
+    private readonly tileId:number;
+
+    private readonly view:React.ComponentClass;
+
+    private readonly model:SydModel;
+
+    private readonly appServices:AppServices;
+
+    constructor(dispatcher:ActionDispatcher, tileId:number, ut:ViewUtils<GlobalComponents>, mainForm:WdglanceMainFormModel,
+            appServices:AppServices, conf:SyDTileConf) {
+        this.tileId = tileId;
+        this.appServices = appServices;
+        this.model = new SydModel(
+            dispatcher,
+            {
+                isBusy: false,
+                error: null
+            },
+            mainForm,
+            new SyDAPI(conf.apiURL)
+        );
+        this.view = viewInit(dispatcher, ut, this.model);
+    }
+
+    init():void {
+
+    }
+
+    getIdent():number {
+        return this.tileId;
+    }
+
+    getView():React.ComponentClass {
+        return this.view;
+    }
+
+    supportsExtendedView():boolean {
+        return false;
+    }
+
+    getLabel():string {
+        return this.appServices.translate('syd_main_label');
+    }
+
+    getQueryTypeSupport(qt:QueryType, lang1:string, lang2?:string):number {
+        if (qt === QueryType.CMP_QUERY) {
+            return 100;
+        }
+        return 0;
+    }
+}
+
+
+export const init:TileFactory.TileFactory<SyDTileConf> = ({
+    tileId, dispatcher, appServices, ut, mainForm, tilesModel, lang1, lang2, conf}) => {
+    return new SyDTile(dispatcher, tileId, ut, mainForm, appServices, conf);
+}
---
+++ src/js/tiles/syd/view.tsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 Tomas Machalek <tomas.machalek@gmail.com>
+ * Copyright 2019 Institute of the Czech National Corpus,
+ *                Faculty of Arts, Charles University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as React from 'react';
+import {Bound} from 'kombo';
+import {ActionDispatcher, ViewUtils} from 'kombo';
+import { GlobalComponents } from '../../views/global';
+import { SydModel, SydModelState } from './model';
+
+
+export function init(dispatcher:ActionDispatcher, ut:ViewUtils<GlobalComponents>, model:SydModel):React.ComponentClass {
+
+    const globComponents = ut.getComponents();
+
+    class SydTileView extends React.PureComponent<SydModelState> {
+
+        render() {
+            return (
+                <globComponents.TileWrapper isBusy={this.props.isBusy} error={this.props.error}
+                        hasData={false}>
+                    <div>Treq - TODO</div>
+                </globComponents.TileWrapper>
+            );
+        }
+    }
+
+    return Bound(SydTileView, model);
+
+}
