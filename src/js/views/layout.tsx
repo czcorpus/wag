1e95b9c769d7697eb1893ac49d33d75f7bd7b585
Tomas Machalek <tomas@machalek.xyz>
2020-05-19 14:20:03 +0200

    Fix #751

--- src/js/views/layout.tsx
+++ src/js/views/layout.tsx
@@ -105,7 +105,7 @@ export function init(ut:ViewUtils<GlobalComponents>):React.SFC<LayoutProps> {
                     <title>{ut.translate('global__wdglance_title')}</title>
                     <link href={`${urlResolve(props.config.hostUrl, 'dist/common.css')}`} rel="stylesheet" type="text/css" />
                     {props.config.favicon ? <link rel="icon" type={props.config.favicon.contentType} href={props.config.favicon.url} /> : null}
-                    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Droid+Sans+Mono%7CRoboto:100,400,400italic,700,700italic%7CRoboto+Condensed:400,700&amp;subset=latin,latin-ext" media="all" />
+                    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Droid+Sans+Mono%7CRoboto:100,400,400italic,700,700italic%7CRoboto+Condensed:400,700&amp;subset=latin,latin-ext&amp;display=swap" media="all" />
                     {props.hostPageEnv.styles.concat(props.config.externalStyles).map(style =>
                        <link key={style} rel="stylesheet" type="text/css" href={style} media="all"/> )}
                 </head>
