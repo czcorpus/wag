2fbde96ef340bf0496a8d46e7749144a49a69491
Martin Zimandl <martin.zimandl@gmail.com>
2025-07-22 12:13:36 +0200

    configurable web socket url

--- webpack.dev.js
+++ webpack.dev.js
@@ -110,7 +110,7 @@ export default (env) => ({
             publicPath: (CONF.develServer || {}).urlRootPath + 'dist/'
         },
         client: {
-            webSocketURL: 'ws://wag.korpus.test/ws', // TODO configurable
+            webSocketURL: (CONF.develServer || {}).webSocketURL || 'ws://wag.korpus.test/ws',
         },
         headers: {
             "Access-Control-Allow-Origin": "*",
