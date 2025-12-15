czcorpus/wag

commit bc4f5ce262f5f936f0db4068d233aa9036a94cb3

Add initial support for sublemmas

src/js/query/index.ts
@@ -95,10 +95,37 @@ export function calcFreqBand(ipm: number): FreqBand {
 }
 
 export interface QueryMatchCore {
-    lemma: string; // space-based value for a multi-word query
-    pos: Array<PosItem>; // each word of a multi-word query
-    upos: Array<PosItem>; // each word of a multi-word query
+    /**
+     * note: space-based value for a multi-word query/match
+     */
+    lemma: string;
+
+    /**
+     * note: not used in case of:
+     * 1) multi-word queries (as we cannot handle all the sublemma
+     *    combinations and the number of variants would grow too much)
+     * 2) grouped (aka "wildcard") matches
+     */
+    sublemma: string | undefined;
+
+    /**
+     * PoS of each word in a (possibly) multi-word query/match
+     */
+    pos: Array<PosItem>;
+
+    /**
+     * UD PoS of each word in a (possibly) multi-word query/match
+     */
+    upos: Array<PosItem>;
+
+    /**
+     * Relative frequency
+     */
     ipm: number;
+
+    /**
+     *
+     */
     flevel: FreqBand | null;
 }
 
@@ -129,6 +156,7 @@ export function findCurrQueryMatch(
         ? srch
         : {
               lemma: undefined,
+              sublemma: undefined,
               word: undefined,
               pos: [],
               upos: [],
@@ -186,6 +214,7 @@ export function addWildcardMatches(qm: Array<QueryMatch>): Array<QueryMatch> {
             if (matches.length > 1) {
                 const wildCard: QueryMatch = {
                     lemma: matches[0].lemma,
+                    sublemma: undefined,
                     pos: [],
                     upos: [],
                     ipm: List.foldl((acc, m) => acc + m.ipm, 0, matches),

src/js/api/vendor/wdglance/similarFreq.ts
@@ -67,6 +67,7 @@ export class SimilarFreqWordsAPI {
                     data.result,
                     List.map((v) => ({
                         lemma: v.lemma,
+                        sublemma: v.sublemma,
                         pos: v.pos,
                         upos: v.upos,
                         ipm: v.ipm,

src/js/models/query.ts
@@ -198,6 +198,7 @@ export class QueryFormModel extends StatelessModel<QueryFormModelState> {
                     state.queryMatches[idx] = state.queryMatches[idx].map(
                         (v, i2) => ({
                             lemma: v.lemma,
+                            sublemma: v.sublemma,
                             word: v.word,
                             pos: v.pos,
                             upos: v.upos,

src/js/server/freqdb/backends/frodo.ts
@@ -4,15 +4,21 @@ import { MainPosAttrValues } from '../../../conf/index.js';
 import { IFreqDB } from '../freqdb.js';
 import { calcFreqBand, QueryMatch } from '../../../query/index.js';
 import { serverHttpRequest } from '../../request.js';
-import { HTTP, List } from 'cnc-tskit';
+import { HTTP, List, pipe } from 'cnc-tskit';
 import { importQueryPosWithLabel } from '../../../postag.js';
 import { SourceDetails } from '../../../types.js';
 import urlJoin from 'url-join';
+
+interface Sublemma {
+    value: string;
+    count: number;
+}
 
 interface HTTPNgramDoc {
     _id: string;
     _rev: string;
     lemma: string;
+    sublemmas: Array<Sublemma>;
     pos: string;
     upos: string;
     count: number;
@@ -62,29 +68,41 @@ export class FrodoClient implements IFreqDB {
             params: {},
         }).pipe(
             map((resp) => {
-                return List.map<HTTPNgramDoc, QueryMatch>(
-                    (v) => ({
-                        word,
-                        lemma: v.lemma,
-                        pos: importQueryPosWithLabel(v.pos, 'pos', appServices),
-                        upos: v.upos
-                            ? importQueryPosWithLabel(
-                                  v.upos,
-                                  'upos',
-                                  appServices
-                              )
-                            : importQueryPosWithLabel(
-                                  v.pos,
-                                  'upos',
-                                  appServices
-                              ),
-                        abs: v.count,
-                        ipm: (v.count / this.corpusSize) * 1e6,
-                        flevel: calcFreqBand((v.count / this.corpusSize) * 1e6),
-                        arf: v.arf,
-                        isCurrent: false,
-                    }),
-                    resp.matches
+                return pipe(
+                    resp.matches,
+                    List.flatMap((v) =>
+                        List.map(
+                            (subl) => ({
+                                word,
+                                lemma: v.lemma,
+                                sublemma: subl.value,
+                                pos: importQueryPosWithLabel(
+                                    v.pos,
+                                    'pos',
+                                    appServices
+                                ),
+                                upos: v.upos
+                                    ? importQueryPosWithLabel(
+                                          v.upos,
+                                          'upos',
+                                          appServices
+                                      )
+                                    : importQueryPosWithLabel(
+                                          v.pos,
+                                          'upos',
+                                          appServices
+                                      ),
+                                abs: v.count,
+                                ipm: (v.count / this.corpusSize) * 1e6,
+                                flevel: calcFreqBand(
+                                    (v.count / this.corpusSize) * 1e6
+                                ),
+                                arf: v.arf,
+                                isCurrent: false,
+                            }),
+                            v.sublemmas
+                        )
+                    )
                 );
             })
         );
@@ -108,22 +126,33 @@ export class FrodoClient implements IFreqDB {
             params: {},
         }).pipe(
             map((resp) => {
-                return List.map<HTTPNgramDoc, QueryMatch>(
-                    (v) => ({
-                        word: '-',
-                        lemma: v.lemma,
-                        pos: importQueryPosWithLabel(v.pos, 'pos', appServices),
-                        upos: importQueryPosWithLabel(
-                            v.upos,
-                            'upos',
-                            appServices
+                return List.flatMap(
+                    (dictEntry) =>
+                        List.map(
+                            (sublemma) => ({
+                                word: '-',
+                                lemma: dictEntry.lemma,
+                                sublemma: sublemma.value,
+                                pos: importQueryPosWithLabel(
+                                    dictEntry.pos,
+                                    'pos',
+                                    appServices
+                                ),
+                                upos: importQueryPosWithLabel(
+                                    dictEntry.upos,
+                                    'upos',
+                                    appServices
+                                ),
+                                abs: dictEntry.count,
+                                ipm: (dictEntry.count / this.corpusSize) * 1e6,
+                                flevel: calcFreqBand(
+                                    (dictEntry.count / this.corpusSize) * 1e6
+                                ),
+                                arf: dictEntry.arf,
+                                isCurrent: false,
+                            }),
+                            dictEntry.sublemmas
                         ),
-                        abs: v.count,
-                        ipm: (v.count / this.corpusSize) * 1e6,
-                        flevel: calcFreqBand((v.count / this.corpusSize) * 1e6),
-                        arf: v.arf,
-                        isCurrent: false,
-                    }),
                     resp.matches
                 );
             })

src/js/tiles/core/wordFreq/similarFreq.ts
@@ -95,13 +95,19 @@ export class SimilarFreqWordsFrodoAPI {
                 map((data) =>
                     pipe(
                         data.matches,
-                        List.map((v) => ({
-                            lemma: v.lemma,
-                            pos: [{ value: v.pos, label: v.pos }], // TODO what about label?
-                            upos: [], // TODO UD support?
-                            ipm: v.ipm,
-                            flevel: calcFreqBand(v.ipm),
-                        })),
+                        List.flatMap((v) =>
+                            List.map(
+                                (sublemma) => ({
+                                    lemma: v.lemma,
+                                    sublemma: sublemma.value,
+                                    pos: [{ value: v.pos, label: v.pos }], // TODO what about label?
+                                    upos: [], // TODO UD support?
+                                    ipm: v.ipm,
+                                    flevel: calcFreqBand(v.ipm),
+                                }),
+                                v.sublemmas
+                            )
+                        ),
                         List.filter(
                             (item) =>
                                 item.lemma !== args.lemma ||

src/js/views/main.tsx
@@ -489,7 +489,12 @@ export function init(
             if (v[props.mainPosAttr].length === 0) {
                 return ut.translate('global__alt_expr_any');
             } else {
-                return List.map((v) => v.label, v[props.mainPosAttr]).join(' ');
+                const pos = List.map((v) => v.label, v[props.mainPosAttr]).join(
+                    ' '
+                );
+                return v.lemma != v.sublemma
+                    ? `${ut.translate('global__srch_variant')} "${v.sublemma}", ${pos}`
+                    : pos;
             }
         };
 

commit f1d17c0531202aee73904c75058b5d518203b67e

Fix typings (caused mainly by rev bc4f5ce262f5f)

src/js/query/index.ts
@@ -106,7 +106,7 @@ export interface QueryMatchCore {
      *    combinations and the number of variants would grow too much)
      * 2) grouped (aka "wildcard") matches
      */
-    sublemma: string | undefined;
+    sublemma: string | null;
 
     /**
      * PoS of each word in a (possibly) multi-word query/match
@@ -156,7 +156,7 @@ export function findCurrQueryMatch(
         ? srch
         : {
               lemma: undefined,
-              sublemma: undefined,
+              sublemma: null,
               word: undefined,
               pos: [],
               upos: [],
@@ -214,7 +214,7 @@ export function addWildcardMatches(qm: Array<QueryMatch>): Array<QueryMatch> {
             if (matches.length > 1) {
                 const wildCard: QueryMatch = {
                     lemma: matches[0].lemma,
-                    sublemma: undefined,
+                    sublemma: null,
                     pos: [],
                     upos: [],
                     ipm: List.foldl((acc, m) => acc + m.ipm, 0, matches),

src/js/server/freqdb/backends/couchdb/index.ts
@@ -228,6 +228,7 @@ export class CouchFreqDB implements IFreqDB {
             List.map<HTTPNgramDoc, QueryMatch>((v) => ({
                 word: word,
                 lemma: v.lemma,
+                sublemma: null,
                 pos: importQueryPosWithLabel(v.pos, 'pos', appServices),
                 upos: importQueryPosWithLabel(v.upos, 'upos', appServices),
                 abs: v.count,
@@ -303,6 +304,7 @@ export class CouchFreqDB implements IFreqDB {
                 List.map(
                     (v) => ({
                         lemma: v.lemma,
+                        sublemma: null,
                         pos: importQueryPosWithLabel(v.pos, 'pos', appServices),
                         upos: importQueryPosWithLabel(
                             v.upos,
@@ -350,6 +352,7 @@ export class CouchFreqDB implements IFreqDB {
                     List.sortBy(([, , form]) => form.count),
                     List.map(([pos, upos, form]) => ({
                         lemma: lemma,
+                        sublemma: null,
                         pos: importQueryPosWithLabel(pos, 'pos', appServices),
                         upos: importQueryPosWithLabel(
                             upos,

src/js/server/freqdb/backends/korpusdb.ts
@@ -318,6 +318,7 @@ export class KorpusFreqDB implements IFreqDB {
                             {
                                 word,
                                 lemma,
+                                sublemma: null, // TODO
                                 pos,
                                 upos: [], // TODO
                                 ipm: 0,
@@ -412,6 +413,7 @@ export class KorpusFreqDB implements IFreqDB {
                                         {
                                             word: curr._name,
                                             lemma: lemma,
+                                            sublemma: null, // TODO
                                             pos: pos,
                                             upos: [], // TODO
                                             ipm: ipm,

src/js/server/routes/main.ts
@@ -144,7 +144,7 @@ export function mkRuntimeClientConf({
                                         item.label
                                     ),
                                     html: value,
-                                    isFooterIntegrated: item.isFooterIntegrated
+                                    isFooterIntegrated: item.isFooterIntegrated,
                                 })
                             )
                         ),
@@ -584,6 +584,7 @@ export function queryAction({
                             return [
                                 {
                                     lemma: null,
+                                    sublemma: null,
                                     word: userConf.queries[queryIdx].word,
                                     pos: [],
                                     upos: [],